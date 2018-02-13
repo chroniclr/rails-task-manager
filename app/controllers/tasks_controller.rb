c724eb8681632145173e359e97dbaa9cd2e8e011
tom vaillant <chroniclr@hushmail.com>
2018-02-13 14:44:12 +0100

    done destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,23 +4,39 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
   def show
     @task = Task.find(params[:id])
   end
 
-  def new
-
-  end
-
   def create
+    @task = Task.new(user_params)
+    @task.save
+    redirect_to show_path(@task)
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(user_params)
+    redirect_to show_path(@task)
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to index_path
+  end
+
+  private
+
+  def user_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #Read
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks', to: 'tasks#index', as: :index
   #Create
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
-
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks', to: 'tasks#update'
-
-  delete 'tasks/:id', to: 'tasks#destroy'
+  #Read
+  get 'tasks/:id', to: 'tasks#show', as: :show
+  #update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: 'update'
+  #delete
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 end
